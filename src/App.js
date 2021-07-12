0de7612a69a4e61eeabd29d51dffadf39c2f9830
Aleksey Sokolov <lionsbet@outlook.com>
2021-07-13 00:28:56 +0300

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppList = () => {
+    const items = ['items 1', 'Items 2', 'Items 3','items 1', 'Items 2', 'Items 3',]
+    return (
+        <ul>
+            {
+                items.map((item, index) => <li key={index}>{item}</li>)
+            }
+        </ul>
+    )
 }
+
+const AppHeader = () => {
+    return (
+        <h1 className="header">Bets nitro</h1>
+    )
+}
+const AppInput = () => {
+    const placeholder = 'Type text...'
+    return (
+        <label>
+            <input
+                placeholder={placeholder}
+                type="text"
+            />
+        </label>
+    )
+}
+const App = () => {
+    return (
+        <>
+            <AppHeader />
+            <AppInput />
+            <AppList />
+        </>
+    )
+}
+
 
 export default App;
